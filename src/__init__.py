61beba90b93e84692b77885f8c68509a487f25c0
KWWnoob <qilin19980601@gmail.com>
2025-09-30 00:12:14 -0400

    Clean version
    clean version for public

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .contact_aware_planar_pcs_dynamics import *
-from .contact_aware_planar_pcs_injury_severity_criterion import *
-from .img_animation import *
-from .planar_contact_geometry import *
-from .planar_pcs_rendering import *
